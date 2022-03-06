0d0ee97db757b02bd970c4ecd49d7433239b9cda
Ashikur38 <mdashikurrahman18@gmail.com>
2022-03-06 22:13:39 +0600

    attendance crud

--- controller/attendance.js
+++ controller/attendance.js
@@ -1,7 +1,9 @@
 const db = require('./../models/index')
 
 exports.index = function(req, res, next) {
-    const attendance = db.Attendance.findAll()
+    const attendance = db.Attendance.findAll({
+            include: { model: db.User, as: "User" }
+        })
         .then((data) => {
             res.json({
                 status: 200,
@@ -11,6 +13,68 @@ exports.index = function(req, res, next) {
         .catch((err) => {
             res.status(500).send({
                 message: err.message || "Err"
+            })
+        })
+}
+exports.store = function(req, res, next) {
+    const attendance = db.Attendance.create({
+            userId: req.body.userId,
+            attendance_date: req.body.attendance_date,
+            clock_in: req.body.clock_in,
+            clock_out: req.body.clock_out,
+            clock_in_out: req.body.clock_in_out,
+            time_late: req.body.time_late,
+            early_leave: req.body.early_leave,
+            overtime: req.body.overtime,
+            total_work: req.body.total_work,
+            total_rest: req.body.total_rest,
+            status: req.body.status
+        })
+        .then((data) => {
+            res.json({
+                status: 200,
+                data: data
+            })
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "ERR"
+            })
+        })
+}
+exports.show = function(req, res, next) {
+    const attendance = db.Attendance.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then((data) => {
+            res.json({
+                status: 200,
+                data: data
+            })
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "Error"
+            })
+        })
+}
+exports.delete = function(req, res, next) {
+    const attendance = db.Attendance.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then((data) => {
+            res.json({
+                status: 200,
+                data: data
+            })
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "err"
             })
         })
 }
--- routes/api.js
+++ routes/api.js
@@ -28,5 +28,8 @@ router.post('/Timesheet/Update/:id', auth.auth, timesheet.update)
 router.get('/Timesheet/Delete/:id', auth.auth, timesheet.delete)
 
 router.get('/Attendance/GetAll', auth.auth, attendance.index)
+router.post('/Attendance/Store', auth.auth, attendance.store)
+router.get('/Attendance/Show/:id', auth.auth, attendance.show)
+router.delete('/Attendance/Delete/:id', auth.auth, attendance.delete)
 
 module.exports = router;
